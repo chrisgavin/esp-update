b672351bc6bf9ebe841793d38b04130b76759be2
Chris Gavin <chris@chrisgavin.me>
2023-06-03 16:06:39 +0100

    Add C++ extern declaration.

--- include/esp_update.h
+++ include/esp_update.h
@@ -3,6 +3,13 @@
 
 #include <esp_err.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 esp_err_t esp_update(const char* update_server, const char* application, const char* current_version);
 
+#ifdef __cplusplus
+}
+#endif
 #endif
